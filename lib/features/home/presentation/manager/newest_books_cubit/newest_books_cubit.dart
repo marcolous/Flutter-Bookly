591f3ef26004685b9e8bed47e4af2100d1f257be
marco <119810706+array-marco@users.noreply.github.com>
2024-04-26 16:22:00 +0300

    create newest books cubit state

---
+++ lib/features/home/presentation/manager/newest_books_cubit/newest_books_cubit.dart
@@ -0,0 +1,11 @@
+import 'package:bookly/features/home/data/models/book_model/book_model.dart';
+import 'package:equatable/equatable.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+part 'newest_books_state.dart';
+
+class NewestBooksCubit extends Cubit<NewestBooksState> {
+  NewestBooksCubit() : super(NewestBooksInitial());
+
+  
+}
