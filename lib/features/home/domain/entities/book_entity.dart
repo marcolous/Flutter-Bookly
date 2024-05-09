042dffad1efa28b97a8128c148873d96b66a39b7
marco <119810706+array-marco@users.noreply.github.com>
2024-05-09 22:49:07 +0300

    book entity and home repo

---
+++ lib/features/home/domain/entities/book_entity.dart
@@ -0,0 +1,11 @@
+class BookEntity {
+  final String image, title, auther;
+  final num price, rating;
+
+  BookEntity(
+      {required this.image,
+      required this.title,
+      required this.auther,
+      required this.price,
+      required this.rating});
+}
